cbed1721373438ecb0bad80d15215c9ecbcc1507
Márton <marton.benko@stud.uni-obuda.hu>
2024-03-11 12:04:51 +0100

    asdasdas

--- modul4/Logic/ChocolateLogic.cs
+++ modul4/Logic/ChocolateLogic.cs
@@ -1,6 +1,9 @@
-﻿using modul4.Models;
+﻿using CommunityToolkit.Mvvm.Messaging;
+
+using modul4.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +13,44 @@ namespace modul4.ChocholateLogic
     public class ChocolateLogic
     {
         IList<Chocolate> chocolates;
+        IMessenger M;
+        private int _income;
         public int Income
         {
             get
             {
-                return chocolates.Count == 0 ? 0 : chocolates.Sum(t => t.Price);
+                return chocolates.Count == 0 ? 0 : chocolates.Sum(t => t.Price * t.Quantity);
+                //return _income;
+            }
+            set
+            {
+                _income = value;
             }
         }
 
+        public ChocolateLogic(IList<Chocolate> l)
+        {
+            this.chocolates = l;
+        }
+
         public void BuySnack(Chocolate c)
+        {
+            c.Quantity -= 1; //TODO: 0 ala nem mehet
+            //Income += c.Price;
+            //M.Send("Buying Snack","SnackInfo");
+        }
+
+        public void AddSnack()
+        {
+
+        }
+
+        public void RemoveSnack(Chocolate c)
+        {
+            chocolates.Remove(c);
+        }
+
+        public void EditSnack()
         {
 
         }
--- modul4/ViewModels/MainWindowViewModel.cs
+++ modul4/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using modul4.ChocholateLogic;
 using modul4.Models;
 using System;
@@ -13,21 +14,31 @@ namespace modul4.ViewModels
 {
     public partial class MainWindowViewModel: ObservableRecipient
     {
-        //public int Income
-        //{
-        //    //get { return logic.Income }
-        //}
-        public ObservableCollection<Chocolate> Chocolates {  get; set; }
-        ChocolateLogic logic;
         public int Income
         {
             get { return logic.Income; }
         }
+        public ObservableCollection<Chocolate> Chocolates {  get; set; }
+        ChocolateLogic logic;
+
+
+        
+        
 
         public ICommand AddButton { get; set; }
         public ICommand RemoveButton { get; set; }
         public ICommand EditButton { get; set; }
         public ICommand BuySnackButton { get; set; }
+        private Chocolate _selectedChocolate;
+        public Chocolate SelectedChocolate 
+        {
+            get { return _selectedChocolate; }
+            set
+            {
+                _selectedChocolate = value;
+                OnPropertyChanged("SelectedChocolate");
+            }
+        }
 
         public MainWindowViewModel()
         {
@@ -50,9 +61,21 @@ namespace modul4.ViewModels
                 Price = 400,
                 Quantity = 16
             });
+            logic = new ChocolateLogic(Chocolates);
+            logic.Income = 4000;
 
+
+            BuySnackButton = new RelayCommand(
+                () => logic.BuySnack(SelectedChocolate)
+                );
+            AddButton = new RelayCommand(
+                () => logic.AddSnack()
+                );
+            RemoveButton = new RelayCommand(
+                () => logic.RemoveSnack(SelectedChocolate)
+                );
 
         }
-        
+
     }
 }
