fa4bc39711218ba9ad7691844b804867b4bd9f7a
Márton <marton.benko@stud.uni-obuda.hu>
2024-03-11 11:13:58 +0100

    Added ICommand buttons

--- modul4/ViewModels/MainWindowViewModel.cs
+++ modul4/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using modul4.ChocholateLogic;
 using modul4.Models;
 using System;
 using System.Collections.Generic;
@@ -6,16 +7,28 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace modul4.ViewModels
 {
-    public class MainWindowViewModel: ObservableRecipient
+    public partial class MainWindowViewModel: ObservableRecipient
     {
         //public int Income
         //{
         //    //get { return logic.Income }
         //}
         public ObservableCollection<Chocolate> Chocolates {  get; set; }
+        ChocolateLogic logic;
+        public int Income
+        {
+            get { return logic.Income; }
+        }
+
+        public ICommand AddButton { get; set; }
+        public ICommand RemoveButton { get; set; }
+        public ICommand EditButton { get; set; }
+        public ICommand BuySnackButton { get; set; }
+
         public MainWindowViewModel()
         {
             Chocolates=new ObservableCollection<Chocolate>();
@@ -37,7 +50,9 @@ namespace modul4.ViewModels
                 Price = 400,
                 Quantity = 16
             });
+
+
         }
-        //asdasdasdasd
+        
     }
 }
